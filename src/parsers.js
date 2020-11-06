a1bd6c17e782fe1a7a357b6656c914ce6e1c8f3b
manyautika <utkina.m@yahoo.com>
2020-11-06 16:36:25 +0300

    update parse module

--- src/parsers.js
+++ src/parsers.js
@@ -3,10 +3,10 @@ import ini from 'ini';
 
 export default (content, format) => {
   const parseFunctions = {
-    '.yaml': yaml.safeLoad,
-    '.yml': yaml.safeLoad,
-    '.json': JSON.parse,
-    '.ini': ini.parse,
+    yaml: yaml.safeLoad,
+    yml: yaml.safeLoad,
+    json: JSON.parse,
+    ini: ini.parse,
   };
   const parseFunction = parseFunctions[format];
   return parseFunction(content);
