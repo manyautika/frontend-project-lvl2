7d57f506a4c47bc7b80467da83865767966eb10c
manyautika <utkina.m@yahoo.com>
2020-11-19 21:37:46 +0300

    github workflows debug

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -13,13 +13,13 @@ const readFixture = (formatter) => {
   return fs.readFileSync(pathToFixture, 'utf-8');
 };
 const fileType = ['json', 'yaml', 'ini'];
+const stylish = readFixture('stylish');
+const plain = readFixture('plain');
+const json = readFixture('json');
 
 test.each(fileType)('format: %s', (extension) => {
   const file1 = getPath(`file1.${extension}`);
   const file2 = getPath(`file2.${extension}`);
-  const stylish = readFixture('stylish');
-  const plain = readFixture('plain');
-  const json = readFixture('json');
   expect(genDiff(file1, file2, 'stylish')).toBe(stylish);
   expect(genDiff(file1, file2, 'plain')).toBe(plain);
   expect(genDiff(file1, file2, 'json')).toBe(json);
