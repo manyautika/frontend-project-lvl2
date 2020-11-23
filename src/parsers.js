c82ac4da07cc802691ee633f34d948d74a9d8407
manyautika <utkina.m@yahoo.com>
2020-11-23 11:38:24 +0300

    update ini parse

--- src/parsers.js
+++ src/parsers.js
@@ -2,21 +2,20 @@ import yaml from 'js-yaml';
 import ini from 'ini';
 import _ from 'lodash';
 
-const iniParse = (rowData) => {
-  const data = ini.parse(rowData);
+const numerifyValues = (obj) => {
+  const result = _.mapValues(obj, (value) => {
+    if (_.isObject(value)) {
+      return numerifyValues(value);
+    }
+    const newValue = parseInt(value, 10);
+    return isNaN(newValue) ? value : newValue; // eslint-disable-line
+  });
+  return result;
+};
 
-  const generate = (obj) => {
-    const result = _.mapValues(obj, (value) => {
-      if (_.isObject(value)) {
-        return generate(value);
-      }
-      const newValue = parseInt(value, 10);
-      return isNaN(newValue) ? value : newValue; // eslint-disable-line
-    });
-    return result;
-  };
-
-  return generate(data);
+const parseINI = (rawData) => {
+  const data = ini.parse(rawData);
+  return numerifyValues(data);
 };
 
 export default (content, format) => {
@@ -24,7 +23,7 @@ export default (content, format) => {
     yaml: yaml.safeLoad,
     yml: yaml.safeLoad,
     json: JSON.parse,
-    ini: iniParse,
+    ini: parseINI,
   };
   const parse = parseFunctions[format];
   return parse(content);
